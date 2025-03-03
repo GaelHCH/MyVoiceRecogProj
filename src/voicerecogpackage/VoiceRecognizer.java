a48cdd5e316f42b39d67f6fedd295f5a70112dc6
GaelHCH <gael.chavez.h@gmail.com>
2025-03-03 00:26:14 -0500

    Have the full english dic and lm, ready for app making stuff

--- src/voicerecogpackage/VoiceRecognizer.java
+++ src/voicerecogpackage/VoiceRecognizer.java
@@ -1,6 +1,9 @@
 package voicerecogpackage;
 
 import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
 import java.io.IOException;
 
 public class VoiceRecognizer {
@@ -17,11 +20,28 @@ public class VoiceRecognizer {
     }
 
     public static void main(String[] args) throws IOException {
-        VoiceRecognizer voiceRecognizer = new VoiceRecognizer();
-        voiceRecognizer.getStartCommand().initConfig();
-        voiceRecognizer.getStartCommand().initRecognizer();
-        voiceRecognizer.getStartCommand().execute();
+//        VoiceRecognizer voiceRecognizer = new VoiceRecognizer();
+//        voiceRecognizer.getStartCommand().initConfig();
+//        voiceRecognizer.getStartCommand().initRecognizer();
+//        voiceRecognizer.getStartCommand().execute();
 
+        Configuration config = new Configuration();
+        config.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        config.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        config.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(config);
+
+        recognizer.startRecognition(true);
+
+        SpeechResult result;
+
+        while ((result = recognizer.getResult()) != null) {
+            String input = result.getHypothesis();
+            System.out.println("Input command: " + input);
+        }
+
+        recognizer.stopRecognition();
     }
 
 
